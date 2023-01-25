8ab4554ba456f0b5fd01b6b375f974263426d451
Grayson <48541040+myaccunt@users.noreply.github.com>
2023-01-25 11:40:34 -0300

    Update UP-scaler.py

--- UP-scaler.py
+++ UP-scaler.py
@@ -6,11 +6,16 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+
+#imagem original para ser processada 
 hr_image = "C:\\Users\\grayson\\Desktop\\12.jpg"
 IMAGE_PATH = "original.png"
+
+#modelo treinado
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
+
 def preprocess_image(image_path):
-  hr_image = tf.image.decode_image(tf.io.read_file(image_path))
+  hr_image = tf.image.decode_image(tf.io.read_file(image_path))  
   # If PNG, remove the alpha channel. The model only supports
   # images with 3 color channels.
   if hr_image.shape[-1] == 4:
@@ -19,12 +24,14 @@ def preprocess_image(image_path):
   hr_image = tf.image.crop_to_bounding_box(hr_image, 0, 0, hr_size[0], hr_size[1])
   hr_image = tf.cast(hr_image, tf.float32)
   return tf.expand_dims(hr_image, 0)
+
 def save_image(image, filename):
   if not isinstance(image, Image.Image):
     image = tf.clip_by_value(image, 0, 255)
     image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
   image.save("%s.jpg" % filename)
   print("Saved as %s.jpg" % filename)
+  
 def plot_image(image, title=""):
   image = np.asarray(image)
   image = tf.clip_by_value(image, 0, 255)
@@ -38,6 +45,8 @@ model = hub.load(SAVED_MODEL_PATH)
 start = time.time()
 fake_image = model(hr_image)
 fake_image = tf.squeeze(fake_image)
+
+#resultado
 print("Time Taken: %f" % (time.time() - start))
 plot_image(tf.squeeze(fake_image), title="Super Resolution")
 save_image(tf.squeeze(fake_image), filename="Super Resolution")
